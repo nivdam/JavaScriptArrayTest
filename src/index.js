963b4ea235e72f6da3f7a3ffa18c1be67ab27383
Niv <nivdam@gmail.com>
2022-04-30 11:45:08 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+/* Sum an Array in a JavaScript  */
+var numberArray = [1, 3, 2, 4];
+var sum = 0;
+var sum2 = 0;
+
+numberArray.forEach(function (val) {
+  // console.log(val + val);
+  sum += val;
+});
+
+for (var i = 0; i < numberArray.length; i++) {
+  sum2 += numberArray[i];
+}
+
+console.log("sum", sum);
+// console.log('sum2', sum2);
+
+/* Sum of number divided by 2 */
+function sumNumbersByDivided(...array) {
+  let sum = 0;
+  let divided = 2;
+
+  /* for(let i = 0; i < numberArray.length; i++) {
+      if (numberArray[i] % divided === 0) {
+        sum += numberArray[i];
+      }
+  } */
+
+  numberArray.forEach(function (val) {
+    if (val % divided == 0) {
+      sum += val;
+    }
+  });
+
+  return sum;
+}
+
+console.log(`divided function ${sumNumbersByDivided(numberArray)}`);
